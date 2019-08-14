a599a601754605d97934690587858fada9ef355f
imunirah <moneerah.md@gmail.com>
2019-08-14 15:54:41 +0300

    Create Components files

---
+++ src/MemeForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function MemeForm(){
+
+return(
+
+<div>
+
+</div>
+
+)
+
+}
+
+export default MemeForm; 
---
+++ src/TextField.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function TextField(){
+
+return(
+
+<div>
+
+</div>
+
+)
+
+}
+
+export default TextField; 
