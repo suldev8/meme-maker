a7b053b626e8d86fcd33cb8171774d886b901c03
Sulaiman Alhabib <suldev8@gmail.com>
2019-08-18 16:14:30 +0300

    add memeForm fucntionality

--- src/components/MemeForm.js
+++ src/components/MemeForm.js
@@ -2,17 +2,92 @@ import React, { Component } from 'react';
 import Image from './common/Image';
 import Button from './common/Button';
 import TextField from './common/TextField';
+import axios from 'axios';
+import { IMAGES_NAMES, HEADERS } from '../data/meme-images'
 
 export class MemeForm extends Component {
+    state = {
+        topValue: '',
+        bottomValue: '',
+        imageSrc: '',
+        imageFile: null
+    };
+    
+    onChangeTopText = e => {
+        this.setState({topValue: e.target.value});
+    }
+
+    onChangeBottomText = e => {
+        this.setState({bottomValue: e.target.value});
+    }
+
+    onChangeFileImage = e => {
+        const imageFile = e.target.files[0];
+        this.setState({ imageFile }, () => console.log(this.state.imageFile));
+    }
+
+    onClickRandom = e => {
+        const randomImageName = IMAGES_NAMES[Math.floor(Math.random() * IMAGES_NAMES.length)];
+        this.getMeme(randomImageName);
+    }
+    onClickUpload = e => {    
+        const { imageFile } = this.state;
+        const imgNameTimeStamp = Date.now() + imageFile.name;
+        const formData = new FormData();
+        formData.append('image', imageFile, imgNameTimeStamp);
+
+        axios.post('https://ronreiter-meme-generator.p.rapidapi.com/images',
+        formData, {
+            headers: 
+            {...HEADERS, 'Content-Type': 'multipart/form-data'}
+        })
+        .then(response => {
+            if(response.data.status === 'success'){
+                this.getMeme(response.data.name);
+            } else if(response.data.status === 'error'){
+                this.getMeme(imgNameTimeStamp);
+            }
+        })
+        .catch(err => console.error(err));
+    }
+    onClickGenerate = e => {
+
+    }
+
+    getMeme(imageName) {
+        console.log(imageName);
+        const { topValue, bottomValue } = this.state;
+        axios({
+            method: 'GET',
+            url: 'https://ronreiter-meme-generator.p.rapidapi.com/meme',
+            headers: HEADERS,
+            responseType: 'arraybuffer',
+            params: {
+                meme: imageName,
+                top: topValue || '.',
+                bottom: bottomValue || '.'
+            }
+        })
+        .then(response => {
+            const bufferImage = Buffer.from(response.data, 'binary').toString('base64');
+            const image = `data:image/jpeg;base64,${bufferImage}`;
+            this.setState({imageSrc: image });
+        })
+        .catch(err => console.error(err))
+    }
+
+
     render() {
+        const { topValue, bottomValue, imageSrc } = this.state;
         return (
             <div>
-                <Image />
-                <TextField label="Top text" name="top-text" />
-                <TextField label="Bottom text" name="bottom-text" />
-                <Button label="Random Meme" />
-                <Button label="Upload Image"/>
-                <Button label="Generate" />
+                <Image src={imageSrc} />
+                <TextField label="Top text" name="top-text" value={topValue} onChange={this.onChangeTopText} />
+                <TextField label="Bottom text" name="bottom-text" value={bottomValue} onChange={this.onChangeBottomText} />
+                <Button label="Random Meme" onClick={this.onClickRandom} />
+                <input type="file" name="MEME image" onChange={this.onChangeFileImage} />
+                <Button label="Upload Image" onClick={this.onClickUpload} />
+                <Button label="Generate" onClick={this.onClickGenerate} />
             </div>
         )
     }
