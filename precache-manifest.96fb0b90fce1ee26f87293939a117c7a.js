1d16fc636a74d4ac8631a107e1b95204f8e067af
Sulaiman Alhabib <suldev8@gmail.com>
2019-08-21 16:04:18 +0300

    Updates

---
+++ precache-manifest.96fb0b90fce1ee26f87293939a117c7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d6dbb89a1d9e8fb535fa4228fb8dc6",
+    "url": "/meme-maker/index.html"
+  },
+  {
+    "revision": "9e326cbcf69bbb0c1815",
+    "url": "/meme-maker/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "984d11aa44305d116384",
+    "url": "/meme-maker/static/css/main.904f42a6.chunk.css"
+  },
+  {
+    "revision": "9e326cbcf69bbb0c1815",
+    "url": "/meme-maker/static/js/2.3b390588.chunk.js"
+  },
+  {
+    "revision": "984d11aa44305d116384",
+    "url": "/meme-maker/static/js/main.79ea6fe6.chunk.js"
+  },
+  {
+    "revision": "d5e8f27c9058884583b4",
+    "url": "/meme-maker/static/js/runtime~main.6eaed0bc.js"
+  },
+  {
+    "revision": "2ebe0df775e2dce1e036721629ce122a",
+    "url": "/meme-maker/static/media/ball-loading.2ebe0df7.svg"
+  }
+]);
