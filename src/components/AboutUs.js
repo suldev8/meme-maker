d9dcf4cd9a53f45d98ab5e8a0a1da59d635fe0ee
imunirah <moneerah.md@gmail.com>
2019-08-21 16:59:13 +0300

    about us

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -6,6 +6,18 @@ const AboutUs = (props) => {
     return (
        <div>
 <h1>About Us</h1>
+<p> This website was created as a part of the learning process at SEI course. The website is for meme maker, the meme maker is an image maker that allows the user to add custom resizable text to images. The developer team tried to develop this meme maker an easy to create, easy to consume, relatable to your audience, shareable, familiar, funny and smart.
+    
+</p>
+<h2>Developer team </h2>
+<ul>
+
+
+<li>Sulaiman Alhabib (suldev8) https://github.com/suldev8</li>
+<li>Munirah Almadi (Imunirah) https://github.com/Imunirah</li>
+<li>Munira AlShuhail (Muniraks) https://github.com/Muniraks</li>
+</ul>
+
        </div>
     )
 }
