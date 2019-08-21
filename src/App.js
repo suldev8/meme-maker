f200acc2cef930622e3f94f374cfb6498cf99dae
Sulaiman Alhabib <suldev8@gmail.com>
2019-08-21 16:24:43 +0300

    Remove Hashrouter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import MemeForm from './components/MemeForm';
 import AboutUs from './components/AboutUs';
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 
 class App extends Component {
